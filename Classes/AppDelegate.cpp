832246c7dc589dc8745680563b5387ae4b894004
DragonheartLX <dh@dragonheartlx.tk>
2021-01-22 14:49:01 +0800

    small changes

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -23,7 +23,7 @@
  ****************************************************************************/
 
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "scenes/HelloWorldScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 
@@ -73,7 +73,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("Drmix", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("Drmix", cocos2d::Rect(0, 0,mediumResolutionSize.width,mediumResolutionSize.height));
 #else
         glview = GLViewImpl::create("Drmix");
 #endif
